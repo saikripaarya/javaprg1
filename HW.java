aeb692bbe3cf64cf4745aba5226cac76e67dc01f
saikripaarya <saikripa.arya@gmail.com>
2020-04-29 02:25:36 +0100

    Next try

--- HW.java
+++ HW.java
@@ -5,7 +5,8 @@ class HW
 	public static void main(String args[])
 	{
 	        System.out.println("Test Jenkins Run");
-		System.out.println("Hello World");
+		System.out.println("Hello World")
+		System.out.println("Next Try");
 	}
 }
 
